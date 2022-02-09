5387d9e68cef6464c32f4d42fc3a92a8a5ad4bdd
Creighton France <tkdg25@gmail.com>
2022-02-09 16:39:26 -0600

    Create initial player model class

---
+++ Photon10/Models/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Photon10.Models
+{
+    public class Player
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [Column(TypeName = "char")]
+        [StringLength(30)]
+        public string first_name { get; set; }
+
+        [Required]
+        [Column(TypeName = "char")]
+        [StringLength(30)]
+        public string last_name { get; set; }
+
+        [Required]
+        [Column(TypeName = "char")]
+        [StringLength(30)]
+        public string codename { get; set; }
+
+    }
+}
