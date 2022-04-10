29e8eef316a508c85f563f4e23a61e9a9384e9e9
Audrey Aaliyah Jensen <isAudball@gmail.com>
2022-04-09 19:45:41 -0500

    implement game manager to host game

---
+++ Photon10/Network/NetworkTrafficListener.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static Photon10.Services.GameHandler;
+
+namespace Photon10.Network
+{
+    public class PacketReceivedEventArgs : EventArgs
+    {
+        public string packetData = string.Empty;
+    }
+
+    public delegate void GamePacketReceived(PacketReceivedEventArgs eventArgs);
+    public static class NetworkTrafficListener
+    {
+        public delegate void processPacket(byte[] bytes);
+        public static event GamePacketReceived EventReceived;
+
+        private static int _port = 7501;
+
+        private static bool stop = false;
+        private static Task listenerThread;
+        public static void Start()
+        {
+            listenerThread = Task.Run(() => StartListening());
+        }
+        private static void StartListening()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, _port);
+            //Create socket
+            UdpClient listener = new(_port);
+            //Bind socket to endpoint
+            try
+            {
+
+                while(!stop)
+                {
+                    byte[] bytes = listener.Receive(ref endPoint);
+                    if(bytes.Length >0)
+                    {
+                        var data = Encoding.UTF8.GetString(bytes);
+                        var args = new PacketReceivedEventArgs
+                        {
+                            packetData = data
+                        };
+                        FirePacketReceived(args);
+                    }
+                }
+                listener.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        
+        public static void FirePacketReceived(PacketReceivedEventArgs e)
+        {
+            if(EventReceived != null)
+                EventReceived(e);
+        }
+        public static void Stop()
+        {
+            stop = true;
+        }
+    }
+}
